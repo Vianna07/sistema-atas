1e6c1bea034842724511228e74dbbcec78c1580c
Vianna07 <viannagustavo71@gmail.com>
2024-09-10 08:14:43 -0300

    first commit

---
+++ src/main/java/com/vianna/sistema_atas/SistemaDeAtasApplication.java
@@ -0,0 +1,13 @@
+package com.vianna.sistema_atas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAtasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAtasApplication.class, args);
+	}
+
+}
